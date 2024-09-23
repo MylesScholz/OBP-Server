b753b803372d67591777813e22036e6ae5143004
MylesScholz <myles.scholz@gmail.com>
2024-09-23 16:41:21 -0700

    Reverting docker-compose.yml to using Dockerfiles, fixed plaintext auth in init-mongo.sh

--- init-mongo.sh
+++ init-mongo.sh
@@ -4,12 +4,12 @@ echo 'init-mongo.sh running...'
 
 set -e
 
-mongosh --username root --password Practicalstoragesystem --authenticationDatabase admin <<EOF
+mongosh --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin <<EOF
 
-use api-backend
+use $MONGO_DB
 
 db.createUser({
-    user: 'api-server',
-    pwd: 'Practicalstoragesystem',
-    roles: [ { role: "readWrite", db: 'api-backend' }]
+    user: $MONGO_USER,
+    pwd: $MONGO_PASSWORD,
+    roles: [ { role: "readWrite", db: $MONGO_DB }]
 })

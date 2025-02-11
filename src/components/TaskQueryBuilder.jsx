f2c5c64717d4d9e9e05eb709e74d3abb2426dc49
MylesScholz <myles.scholz@gmail.com>
2025-02-10 16:18:17 -0800

    Source project search form

--- src/components/TaskQueryBuilder.jsx
+++ src/components/TaskQueryBuilder.jsx
@@ -2,6 +2,121 @@ import { useState } from 'react'
 import axios from 'axios'
 import styled from '@emotion/styled'
 
+const ProjectSelectionContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    .project {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 10px;
+
+        border: 1px solid gray;
+        border-radius: 5px;
+
+        padding: 5px;
+        
+        p {
+            margin: 0px;
+        }
+
+        button {
+            margin: 0px;
+
+            padding: 5px;
+
+            width: 30px;
+            height: 30px;
+
+            text-align: center;
+        }
+    }
+
+    #addProjectForm {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        gap: 10px;
+
+        border: 1px solid gray;
+        border-radius: 5px;
+
+        padding: 5px;
+
+        #projectNameQuery {
+            flex-grow: 1;
+        }
+
+        #submitProject {
+            margin: 0px;
+
+            padding: 5px;
+
+            width: 30px;
+            height: 30px;
+
+            text-align: center;
+        }
+    }
+`
+
+function ProjectSelection() {
+    const defaultProjects = [
+        { name: 'Oregon Bee Atlas', id: '18521' },
+        { name: 'Master Melittologist (outside of Oregon)', id: '99706' },
+        { name: 'Washington Bee Atlas', id: '166376' }
+    ]
+    const [ projects, setProjects ] = useState(defaultProjects)
+    const [ newProjectQuery, setNewProjectQuery ] = useState('')
+
+    function handleRemove(projectId) {
+        setProjects(projects.filter((p) => p.id !== projectId))
+    }
+
+    async function handleAdd() {
+        const trimmedQuery = newProjectQuery.trim()
+        if (trimmedQuery) {
+            const queryURL = `https://api.inaturalist.org/v1/projects/autocomplete?q=${trimmedQuery}`
+            const res = await axios.get(queryURL)
+            const topResult = res?.data?.results?.at(0)
+
+            if (topResult && projects.every((p) => p.id != topResult.id)) {
+                setProjects([...projects, { name: topResult.title, id: topResult.id }])
+            }
+            
+            setNewProjectQuery('')
+        }
+    }
+
+    return (
+        <ProjectSelectionContainer>
+            <label for='sources'>iNaturalist Projects:</label>
+            <input type='hidden' id='sources' value={projects.map((p) => p.id).join(',')} />
+
+            { projects.map((project) => (
+                <div key={project.id} className='project'>
+                    <p>{project.name} (ID: {project.id})</p>
+                    <button onClick={ () => handleRemove(project.id) }>X</button>
+                </div>
+            )) }
+
+            <div id='addProjectForm'>
+                <input
+                    type='text'
+                    id='projectNameQuery'
+                    placeholder='Search for an iNaturalist Project...'
+                    value={newProjectQuery}
+                    onChange={(e) => setNewProjectQuery(e.target.value)}
+                    onKeyDown={(e) => { if (e.key === 'Enter') { handleAdd() } }}
+                />
+                <button type='button' id='submitProject' onClick={ handleAdd }>+</button>
+            </div>
+        </ProjectSelectionContainer>
+    )
+}
+
 const TaskQueryBuilderContainer = styled.div`
     display: flex;
     flex-grow: 1;
@@ -125,18 +240,15 @@ export default function TaskQueryBuilder({ setQueryResponse, setResult, formDisa
                             </div>
                             { pullSources === 'yes' &&
                                 <>
-                                    <div>
-                                        <label for='sources'>iNaturalist Project Source IDs:</label>
-                                        <input type='text' id='sources' required />
-                                    </div>
+                                    <ProjectSelection />
 
                                     <div>
-                                        <label for='minDate'>Min Date:</label>
+                                        <label for='minDate'>Minimum Date:</label>
                                         <input type='date' id='minDate' required />
                                     </div>
 
                                     <div>
-                                        <label for='maxDate'>Max Date:</label>
+                                        <label for='maxDate'>Maximum Date:</label>
                                         <input type='date' id='maxDate' required />
                                     </div>
                                 </>

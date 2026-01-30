37d3892ddde6924530fec5b768c889f779656514
MylesScholz <myles.scholz@gmail.com>
2026-01-30 15:34:37 -0800

    Periodic login check query

--- client/src/AuthProvider.jsx
+++ client/src/AuthProvider.jsx
@@ -1,9 +1,27 @@
 import { createContext, useContext, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 const AuthContext = createContext()
 
 export function AuthProvider({ children }) {
     const [ loggedIn, setLoggedIn ] = useState(null)
+
+    useQuery({
+        queryKey: ['loggedInQuery'],
+        queryFn: async () => {
+            axios.get('/api/admins/login').then((res) => {
+                if (res.status === 200) {
+                    setLoggedIn(res.data.username)
+                } else {
+                    setLoggedIn(null)
+                }
+            })
+            return
+        },
+        refetchInterval: 300000,    // 5 minutes
+        refetchOnMount: 'always'
+    })
 
     return (
         <AuthContext.Provider value={{ loggedIn, setLoggedIn }}>

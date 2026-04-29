f9839f8533237be6a8df59c812e0a3f2ccfbc695
Nora Jacobi <8314702+AnEntireHam@users.noreply.github.com>
2026-04-29 14:11:38 -0700

    Proper EmailsSubtaskHandler fix

--- shared/lib/repositories/OccurrenceRepository.js
+++ shared/lib/repositories/OccurrenceRepository.js
@@ -308,7 +308,14 @@ export default class OccurrenceRepository extends BaseRepository {
     async updateById(id, updateDocument = {}, options = {}) {
         // Find the existing document and assign the update values to it
         const document = await this.findById(id)
-        if (!document) return
+        if (!document && options.upsert === true) {
+            console.error("Creating new occurrence with ID: ", id)
+            return this.create(updateDocument)
+
+        } else if (!document) {
+            console.error("Couldn't find the id: ", id)
+            return
+        }
         
         let processedDocument = Object.assign(document, updateDocument)
 
--- worker/src/handlers/EmailsSubtaskHandler.js
+++ worker/src/handlers/EmailsSubtaskHandler.js
@@ -163,14 +163,13 @@ export default class EmailsSubtaskHandler extends BaseSubtaskHandler {
         // Delete old scratch space occurrences (from previous tasks)
         await OccurrenceService.deleteOccurrences({ scratch: true })
 
-        // BODGE: We'll always use the whole DB for this step
-        // if (subtask.input !== 'selection') {
-        //     // Upsert data from the input occurrence file into scratch space (existing records will be moved to scratch space)
-        //     await OccurrenceService.upsertOccurrencesFromFile(inputFilePath, { scratch: true })
-        // } else {    // subtask.input === 'selection'
-        //     // Move occurrences matching the query parameters into scratch space
-        //     await OccurrenceService.updateOccurrences(subtask.params?.filter ?? {}, { scratch: true })
-        // }
+        if (subtask.input !== 'selection') {
+            // Upsert data from the input occurrence file into scratch space (existing records will be moved to scratch space)
+            await OccurrenceService.upsertOccurrencesFromFile(inputFilePath, { scratch: true })
+        } else {    // subtask.input === 'selection'
+            // Move occurrences matching the query parameters into scratch space
+            await OccurrenceService.updateOccurrences(subtask.params?.filter ?? {}, { scratch: true })
+        }
 
         await TaskService.logTaskStep(taskId, 'Compiling user email addresses')
 
@@ -179,9 +178,7 @@ export default class EmailsSubtaskHandler extends BaseSubtaskHandler {
         const userLogins = users.map((user) => user[usernames.fieldNames.userLogin])
                                 .filter((userLogin) => !!userLogin)
         
-        // BODGE: We'll always use the whole DB for this step
-        // const userErrors = await OccurrenceService.getErrorFlagsByUserLogins(userLogins, { scratch: true })
-        const userErrors = await OccurrenceService.getErrorFlagsByUserLogins(userLogins)
+        const userErrors = await OccurrenceService.getErrorFlagsByUserLogins(userLogins, { scratch: true })
 
         // Construct a map of each user login to its corresponding error flags (as an Array)
         const userErrorMap = this.#buildUserErrorMap(userErrors)
@@ -204,18 +201,17 @@ export default class EmailsSubtaskHandler extends BaseSubtaskHandler {
         // Archive excess output files
         FileManager.limitFilesInDirectory('./shared/data/emails', fileLimits.maxEmails)
 
-        // BODGE: We'll always use the whole DB for this step
         // Move occurrences with a fieldNumber or no errorFlags back to non-scratch space
-        // const unscratchFilter = {
-        //     scratch: true,
-        //     $or: [
-        //         { [fieldNames.fieldNumber]: { $exists: true, $nin: [ null, '' ] } },
-        //         { [fieldNames.errorFlags]: { $exists: false } },
-        //         { [fieldNames.errorFlags]: { $in: [ null, '' ] } }
-        //     ]
-        // }
-        // await OccurrenceService.updateOccurrences(unscratchFilter, { scratch: false })
-        // // Discard remaining scratch space occurrences
-        // await OccurrenceService.deleteOccurrences({ scratch: true })
+        const unscratchFilter = {
+            scratch: true,
+            $or: [
+                { [fieldNames.fieldNumber]: { $exists: true, $nin: [ null, '' ] } },
+                { [fieldNames.errorFlags]: { $exists: false } },
+                { [fieldNames.errorFlags]: { $in: [ null, '' ] } }
+            ]
+        }
+        await OccurrenceService.updateOccurrences(unscratchFilter, { scratch: false })
+        // Discard remaining scratch space occurrences
+        await OccurrenceService.deleteOccurrences({ scratch: true })
     }
 }
